3606c8fcdf77659454c1c4c613194221bbab804f
t.tahmazov <t.tahmazov@H1-F3IT-DEV-D24>
2023-10-05 12:21:45 +0400

    Entities from Abstract AuditableEntity is created

---
+++ Entities/Concrete/BaseEntities/AuditableEntity.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete.BaseEntity
+{
+    public class AuditableEntity : BaseEntity, ICreatedEntity,IUpdatedEntity
+    {
+        public int CretaedUserId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime CreatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int? UpdatedUserId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime? UpdatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
