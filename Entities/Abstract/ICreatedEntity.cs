96c9d5c9a5a23fb057af383a5dc1180fcbef890c
t.tahmazov <t.tahmazov@H1-F3IT-DEV-D24>
2023-10-05 12:15:04 +0400

    Entities from Abstract ICreatedEntity has been created

---
+++ Entities/Abstract/ICreatedEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Entities.Abstract
+{
+    public  interface ICreatedEntity
+    {
+        int CretaedUserId { get; set; }
+        DateTime CreatedDate { get; set; }
+    }
+}
